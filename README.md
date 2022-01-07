edc8c108af5e7e5a61431134cbad5db33c30f2d8
Peter Chu <peterc0523@gmail.com>
2022-01-07 09:44:48 -0500

    first functional version, yaygit add .

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # rock-paper-scissors
 theOdinProject: Rock Paper Scissors
+
+Hello, thanks for checking out my Rock Paper Scissors project!
+
+This will demonstrate my ability to use the following:
+Git
+GitHub
+Terminal
+VS Code
+JavaScript
+HTML
+Problem Solve
---
+++ index.js
@@ -0,0 +1,54 @@
+//Problem:  Design a Rock Paper Scissors game
+//Plan:  Use random numbers 1-3; assign numbers to R/P/S;
+//Divide:  Prompt user to pick R/P/S; compare against computers random pick; console.log result
+
+// let compPlay = "";
+// let userPlay = ""; 
+
+
+function computerPlay() {
+    let compPlay = "";
+    let userPlay = "";
+
+     function play() {
+        userPlay = prompt("Enter: Rock, Paper, or Scissors");
+        userPlay = userPlay.charAt(0).toUpperCase() + userPlay.slice(1);
+        console.log(`Player chooses ${userPlay}`);
+        if(userPlay !== "Rock" && userPlay !== "Paper" && userPlay !== "Scissors") {
+            alert("Please enter a valid option!");
+            play();
+        } else {
+        let randomPlay = Math.floor(Math.random() * 3 ) + 1;
+        if(randomPlay === 1) {
+            compPlay = "Rock";
+            console.log(`Computer chooses ${compPlay}`);
+            compare();
+        } else if(randomPlay === 2) {
+            compPlay = "Paper";
+            console.log(`Computer chooses ${compPlay}`);
+            compare();
+        } else {
+            compPlay = "Scissors";
+            console.log(`Computer chooses ${compPlay}`);
+            compare(); 
+        }
+    }
+
+    function compare() {
+        console.log(`Comp = ${compPlay} & Player = ${userPlay}`);
+        if(compPlay == userPlay) {
+            console.log(`It's a tie! Computer chose ${compPlay}.  Player chose ${userPlay}.`);
+            alert(`It's a tie! Computer chose ${compPlay}.  Player chose ${userPlay}.`);
+            return;
+        } else if((compPlay == "Rock" && userPlay == "Scissors") || (compPlay == "Paper" && userPlay == "Rock") || (compPlay == "Scissors" && userPlay == "Rock")) {
+            console.log(`Computer wins!  Computer chose ${compPlay}.  Player chose ${userPlay}.`);
+            alert(`Computer wins!  Computer chose ${compPlay}.  Player chose ${userPlay}.`);
+            return;
+        } else
+            console.log(`Player wins!  Computer chose ${compPlay}.  Player chose ${userPlay}.`);
+            alert(`Player wins!  Computer chose ${compPlay}.  Player chose ${userPlay}.`);
+            return;
+        }
+    }
+    play();
+}
