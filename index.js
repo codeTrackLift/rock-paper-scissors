accbd7e482d690f777c955d264324e482cc67f5e
Peter Chu <peterc0523@gmail.com>
2022-01-08 09:16:39 -0500

    added stats and styling

--- index.js
+++ index.js
@@ -2,9 +2,14 @@
 //Plan:  Use random numbers 1-3; assign numbers to R/P/S;
 //Divide:  Prompt user to pick R/P/S; compare against computers random pick; console.log result
 
+let rounds = 0;
+let win = 0;
+let lose = 0;
+let tie = 0;
+
 function game() {
     let resultsArr = [];
-    for(let rounds = 1; rounds < 6; rounds++) {
+    for(let rounds = 1; rounds < 4; rounds++) {
         document.getElementById("rounds").innerHTML = rounds;
         function computerPlay() {
             let compPlay = "";
@@ -33,25 +38,31 @@ function game() {
             function compare() {
                 console.log(`Comp = ${compPlay} & Player = ${userPlay}`);
                 if(compPlay == userPlay) {
-                    var result = "It's a tie! Computer: " + compPlay + ",  Player: "  + userPlay;
-                    resultsArr.push("<br>" + result);
+                    var result = "<strong><u>It's a tie.</u></strong>  Computer: " + compPlay + ",  Player: "  + userPlay;
+                    resultsArr.push("<br><br>" + result);
+                    tie++;
                     document.getElementById("result").innerHTML = resultsArr;
+                    document.getElementById("tie").innerHTML = tie;
                     console.log(result);
-                    alert(result);
+                    alert("This round is a tie.");
                     return;
                 } else if((compPlay == "Rock" && userPlay == "Scissors") || (compPlay == "Paper" && userPlay == "Rock") || (compPlay == "Scissors" && userPlay == "Paper")) {
-                    var result = "Computer wins!  Computer: " + compPlay + ",  Player: "  + userPlay;
-                    resultsArr.push("<br>" + result);
+                    var result = "<strong><u>You lose.</u></strong>  Computer: " + compPlay + ",  Player: "  + userPlay;
+                    resultsArr.push("<br><br>" + result);
+                    lose++;
                     document.getElementById("result").innerHTML = resultsArr;
+                    document.getElementById("lose").innerHTML = lose;
                     console.log(result);
-                    alert(result);
+                    alert("You lost this round.");
                     return;
                 } else {
-                    var result = "Player wins!  Computer: " + compPlay + ",  Player: " + userPlay; 
-                    resultsArr.push("<br>" + result);
+                    var result = "<strong><u>You win!</u></strong>  Computer: " + compPlay + ",  Player: " + userPlay; 
+                    resultsArr.push("<br><br>" + result);
+                    win++;
                     document.getElementById("result").innerHTML = resultsArr;
+                    document.getElementById("win").innerHTML = win;
                     console.log(result);
-                    alert(result);
+                    alert("You won this round!");
                     return;
                     }
                 }
@@ -65,13 +76,12 @@ function game() {
 //Problem: Modify RPS game to use button interface rather than text prompts
 //Plan: Button enters userPlay variable, compare, print results to array, subsequent button press increments rounds and adds to results array
 
-let rounds = 0;
 function buttonPlay(value) {
     let resultsArr = [];
     console.log(`User clicked the ${value} button`);
     userPlay = value;
     let randomPlay = Math.floor(Math.random() * 3 ) + 1;
-    rounds += 1;
+    rounds++;
     document.getElementById("rounds").innerHTML = rounds;
     if(randomPlay === 1) {
         compPlay = "Rock";
@@ -85,21 +95,27 @@ function buttonPlay(value) {
     function compare() {
         console.log(`Comp = ${compPlay} & Player = ${userPlay}`);
         if(compPlay == userPlay) {
-            var result = "<b><u>It's a tie!</u></b> <br>Computer: " + compPlay + "<br>Player: "  + userPlay;
+            var result = "<b><u>It's a tie.</u></b> <br>Computer: " + compPlay + "<br>Player: "  + userPlay;
             resultsArr.push(result);
+            tie++;
             document.getElementById("result").innerHTML = resultsArr;
+            document.getElementById("tie").innerHTML = tie;
             console.log(result);
             return;
         } else if((compPlay == "Rock" && userPlay == "Scissors") || (compPlay == "Paper" && userPlay == "Rock") || (compPlay == "Scissors" && userPlay == "Paper")) {
-            var result = "<b><u>Computer wins!</u></b> <br>Computer: " + compPlay + "<br>Player: "  + userPlay;
+            var result = "<b><u>You lose.</u></b> <br>Computer: " + compPlay + "<br>Player: "  + userPlay;
             resultsArr.push(result);
+            lose++;
             document.getElementById("result").innerHTML = resultsArr;
+            document.getElementById("lose").innerHTML = lose;
             console.log(result);
             return;
         } else {
             var result = "<b><u>Player wins!</u></b> <br>Computer: " + compPlay + "<br>Player: " + userPlay; 
             resultsArr.push(result);
+            win++;
             document.getElementById("result").innerHTML = resultsArr;
+            document.getElementById("win").innerHTML = win;
             console.log(result);
             return;
             
@@ -107,8 +123,14 @@ function buttonPlay(value) {
     }
 }
 
-//button to reset rounds
-function resetRounds() {
+//button to reset stats
+function resetStats() {
     rounds = 0;
     document.getElementById("rounds").innerHTML = rounds;
+    win = 0;
+    document.getElementById("win").innerHTML = win;
+    lose = 0;
+    document.getElementById("lose").innerHTML = lose;
+    tie = 0;
+    document.getElementById("tie").innerHTML = tie;
 }
